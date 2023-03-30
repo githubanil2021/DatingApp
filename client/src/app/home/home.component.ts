78e35721dd52c7271c52e1cb40000f02ae963154
Anil Kumar <akumar.contact@gmail.com.com>
2023-03-30 18:22:50 +0530

    End of 9

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -14,21 +15,21 @@ export class HomeComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.getUsers();
+    // this.getUsers();
   }
 
   registerToggle(){
     this.registerMode = !this.registerMode;
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Complete')
+  // getUsers(){
+  //   this.http.get(environment.apiUrl+ '/users').subscribe({
+  //     next: response => this.users = response,
+  //     error: error => console.log(error),
+  //     complete: () => console.log('Complete')
 
-    });
-   }
+  //   });
+  //  }
 
    cancelRegisterMode(event: boolean){
     this.registerMode = event;
